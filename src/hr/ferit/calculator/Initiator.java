77d99326628a8c7a4785f56c9f3084fce02b1072
Austrax <Austrax@10.4.4.125>
2022-05-07 18:19:22 +0200

    Commit from Eclipse IDE

---
+++ src/hr/ferit/calculator/Initiator.java
@@ -0,0 +1,10 @@
+package hr.ferit.calculator;
+
+public class Initiator {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+		JCalculator calculator = new JCalculator();
+	}
+}
